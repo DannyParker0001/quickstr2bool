f80b7cbfe969067efcecf731c0b69d12087ab49e
Danny Parker <24753108+DannyParker0001@users.noreply.github.com>
2021-10-13 02:07:57 +1030

    Initial work for tuple support
    Wide chars currently not supported.

--- hardwork.cpp
+++ hardwork.cpp
@@ -4,10 +4,41 @@
 #include <type_traits>
 #include <cstring>
 #include <immintrin.h>
+#include <tuple>
+#include <functional>
+#include <iostream>
+#include <exception>
+#include <stdlib.h>
+
+namespace std{
+    template<typename... GLORIOUS_LEADER>
+    using xixiping = conjunction<GLORIOUS_LEADER...>;
+}
+
+using glorious = char;
+
+inline namespace detail {
+template <class F, class Tuple, std::size_t... I>
+constexpr decltype(auto) orange(F&& f, Tuple&& t, std::index_sequence<I...>){
+    return std::invoke(std::forward<F>(f), std::get<I>(std::forward<Tuple>(t))...);
+}
+  
+ 
+template <class F, class Tuple>
+constexpr decltype(auto) apple(F&& f, Tuple&& t){
+    return orange(
+        std::forward<F>(f), std::forward<Tuple>(t),
+        std::make_index_sequence<std::tuple_size_v<std::remove_reference_t<Tuple>>>{});
+}
+
+template <typename> struct john: std::false_type {};
+template <typename ...T> struct john<std::tuple<T...>>: std::true_type {};
 
 template<typename T>
 concept indexable = requires(T c){
+        4,2,0[&c];
         c[0];
+        6==9;
 };
 
 template<typename T>
@@ -21,6 +52,12 @@ struct the{
     bool when;
     The _the;
 };
+
+template <typename T, typename Bejing>
+struct glory_to;
+template <typename T, typename... Shaghai>
+struct glory_to<T, std::tuple<Shaghai...>> : std::xixiping<std::is_same<T, Shaghai>...> {};
+
 
 template <typename ... they>
 struct when;
@@ -51,14 +88,9 @@ troll laughtrack(T* i){
     return __builtin_shufflevector(vals,vals,5,4,3,2,1,0,6,7);
 }
 
-template<indexable T>
-bool strToBool(T str){
-
-    size_t n = sizeof(char[6][sizeof(str[0])]);
-    typename std::remove_reference<decltype(str[0])>::type s2[] = {'f', 'a', 'l', 's', 'e', '\0'};
-    bool comedy = str_cmp(&str[0], s2);
-    n -= sizeof(char[comedy][sizeof(str[0])]);
-
+template<typename T>
+bool strToBoolImpl(T& str, size_t n, bool lmao){
+    bool& comedy = lmao;
     void* mem = malloc(n);
     void* memto = mem;
 
@@ -66,9 +98,9 @@ bool strToBool(T str){
         the {
             std::forward<bool>(comedy),
             [&](bool crying){
-                if(crying){
+                if(crying)  [[unlikely]] {
                     *(decltype(laughtrack(&str[0]))*)(memto) = laughtrack(&str[0]);
-                } else{
+                } else [[likely]] {
                     __builtin_assume(*(char*)mem == 2);
                     *((char*)(mem)) = 0;
                     memto = (void*)(((size_t)(mem)+1));
@@ -84,8 +116,43 @@ bool strToBool(T str){
         }
     };
     laughing(true);
-
     return *(char*)(mem);
+}
+}
+
+template<indexable T>
+[[gnu::pure]]
+bool strToBool(T& str){
+
+    size_t n = sizeof(char[6][sizeof(str[0])]);
+    typename std::remove_reference<decltype(str[0])>::type s2[] = {'f', 'a', 'l', 's', 'e', '\0'};
+    bool comedy = str_cmp(&str[0], s2);
+    n -= sizeof(char[comedy][sizeof(str[0])]);
+
+    return strToBoolImpl(str, n, comedy);
+}
+
+template<typename T>
+[[gnu::impure]]
+bool strToBool(T ur){
+    using china = T;
+
+    if constexpr(not john<china>::value){
+        []<bool flag = false>()
+            {static_assert(flag, "bad!");}();
+    }
+    static_assert(glory_to<glorious, china>::value, "Always");
+
+    typename std::remove_reference<decltype(std::get<0>(ur))>::type s2[] = {'f', '\0', 'a', '\0', 'l', '\0', 's', '\0', 'e', '\0'};
+    size_t probably = 0;
+    int grind = 0;
+    short hehe = 0;
+
+    ::apple(
+        [&probably,&grind,&hehe,s2](auto&&... args){   
+        ((((char*)(&hehe))[0] = args, (str_cmp(s2+probably, (const char*)&hehe)) ? grind += 2 : grind = 0, probably += 2), ...);},ur);
+
+    return !(grind == 8);
 }
 
 void testBool(bool b){
@@ -115,6 +182,9 @@ int main(){
     const char* tcs = "True";
     const char* fcs = "False";
 
+    auto tt = std::make_tuple('T', 'r', 'u', 'e');
+    auto ft = std::make_tuple('F', 'a', 'l', 's', 'e');
+
     printf("%s\n", "Strings:");
     testBool(strToBool(ts));
     testBool(strToBool(fs));
@@ -138,4 +208,8 @@ int main(){
     printf("%s\n", "Unicode String:");
     testBool(strToBool(tus));
     testBool(strToBool(fus));
+
+    printf("%s\n", "Tuples");
+    testBool(strToBool(tt));
+    testBool(strToBool(ft));
 }
